d6241f0bbe3b1e97855825cababf4aec9d918d9b
jamesvu725 <james.vu725@csu.fullerton.edu>
2019-04-18 11:17:05 -0700

    Added test, greet, and help commands to bot.js

--- Source/bot.js
+++ Source/bot.js
@@ -1,39 +1,54 @@
+// Check if required files exists
 var Discord = require('../Libs/node_modules/discord.io');
 var logger = require('../Libs/node_modules/winston');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+	colorize: true
 });
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+	token: auth.token,
+	autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+	// Our bot needs to know if it will execute a command
+	// It will listen for messages that will start with `!`
+	if (message.substring(0, 1) == '!') {
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+		args = args.splice(1);
+		// List of commands
+		switch(cmd) {
+			// !test
+			case 'test':
+				bot.sendMessage({
+					to: channelID,
+					message: 'This is a test!'
+				});
+			break;
+			// !greet
+			case 'greet':
+				bot.sendMessage({
+					to: channelID,
+					message: 'Hello, this is ' + bot.username + ' bot!'
+				});
+			break;
+			// !help
+			case 'help':
+				bot.sendMessage({
+					to: channelID,
+					message: '```\nMapleBot help page\n```'
+				});
+			break;
+			// Just add any case commands if you want to..
+		}
+	}
 });
