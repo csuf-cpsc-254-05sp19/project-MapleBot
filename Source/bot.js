a22e0939923313f5832532166f65f48ed36df246
jamesvu725 <james.vu725@csu.fullerton.edu>
2019-05-05 15:50:32 -0700

    Switched dependencies from discord.io to discord.js. Reworked parts of bot.js and updated more functions. Updated proposal to reflect dependency change.

--- Source/bot.js
+++ Source/bot.js
@@ -1,69 +1,84 @@
-// Check if required files exists
-var Discord = require('../Libs/node_modules/discord.io');
-var logger = require('../Libs/node_modules/winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-	colorize: true
+// check for required files
+const Discord = require('../Libs/node_modules/discord.js');
+const auth = require('./auth.json');
+// Create the client
+const client = new Discord.Client();
+// Makes sure bot is ready
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ game: { name: '!help' } });
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true
-});
-bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-	// The bot needs to know if it will execute a command
-	// It will listen for messages that will start with `!`
-	if (message.substring(0, 1) == '!') {
-		var args = message.substring(1).split(' ');
-		var cmd = args[0];
-		args = args.splice(1);
-		// List of commands
-		switch(cmd) {
-			// !clean
-			case 'clean':
-				bot.sendMessage({
-					to: channelID,
-					message: 'This should clean up some messages.'
-				});
-			break;
-			// !greet
-			case 'greet':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Hello, this is ' + bot.username + ' bot!'
-				});
-			break;
-			// !help
-			case 'help':
-				bot.sendMessage({
-					to: channelID,
-					message: `\`\`\`css
-[MapleBot help page]
-!command [required] Example of how command should be inputted
 
-!clean   [number]   Cleans up n numbers of messages
+// Commands for bot, prefix is '!'
+client.on('message', msg => {
+  if (msg.content.substring(0,1) == '!') {
+    let args = msg.content.substring(1).split(' ');
+    let cmd = args[0];
+    args = args.splice(1);
+    // List of commands
+    switch (cmd) {
+      // Clears all messages that are newer than 14 days
+      case 'clear':
+        if (msg.member.hasPermission("MANAGE_MESSAGES")) {
+          msg.channel.fetchMessages().then(function(list) {
+            msg.channel.bulkDelete(list);
+          }, function(err) {msg.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+        }
+        msg.channel.send('All messages newer than 14 days were cleared!');
+        break;
+
+      // Displays an example of how to use commands
+      case 'command':
+        msg.channel.send(`
+\`\`\`css
+Example of command:
+[prefix][command]
+!help
+\`\`\`
+          `);
+        break;
+
+      // Displays greet message
+      case 'greet':
+        msg.reply(`Hi, this is ${client.user.tag}!`);
+        break;
+
+      // Returns the help page
+      case 'help':
+        msg.channel.send(`
+\`\`\`css
+[MapleBot help page]
+!clear              Clear all messages that are newer than 14 days
+!command            Displays an example of how to use commands
 !greet              Displays greet message
 !help               Display this help page
+!ping               Returns the ping
 !rank    [name]     Displays character ranking
-\`\`\``
-				});
-			break;
-			// !rank
-			case 'rank':
-				bot.sendMessage({
-					to: channelID,
-					message: 'This should display ranking of character.'
-				});
-			break;
-			// Just add any case commands if you want to. Don't forget to put the break
-		}
-	}
+\`\`\`
+`);
+        break;
+
+      // Returns the ping
+      case 'ping':
+        let start = msg.createdTimestamp;
+        msg.channel.send('MapleBot responded').then((msg) => {
+          let diff = Math.abs(msg.createdTimestamp - start);
+          msg.edit(`MapleBot responded in *${diff/1000} seconds*`);
+        }).catch((error) => console.log(error));
+        break;
+
+      // Displays character rank
+      case 'rank':
+        msg.channel.send('This should display ranking of character.');
+        break;
+
+      // Default does nothing if command is incorrect
+      default:
+        break;
+    }
+  }
 });
+
+
+// Bot login
+client.login(auth.token);
